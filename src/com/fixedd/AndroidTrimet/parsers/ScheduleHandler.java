f273d729193866f59eb85114eeb61dd4d7353ae0
Jeremy Logan <jeremy.logan@gmail.com>
2009-12-20 20:38:05 -0800

    Adding the Schedule xml parsing handler for routeConfig and detours stops.

---
+++ src/com/fixedd/AndroidTrimet/parsers/ScheduleHandler.java
@@ -0,0 +1,94 @@
+package com.fixedd.AndroidTrimet.parsers;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import android.util.Log;
+
+import com.fixedd.AndroidTrimet.schemas.Schedule.DetourType;
+import com.fixedd.AndroidTrimet.schemas.Schedule.ResultSet;
+import com.fixedd.AndroidTrimet.schemas.Schedule.RouteDirectionType;
+import com.fixedd.AndroidTrimet.schemas.Schedule.RouteType;
+import com.fixedd.AndroidTrimet.schemas.Schedule.StopType;
+
+public class ScheduleHandler extends DefaultHandler {
+	protected ResultSet          mResultSet        = null;
+	private   RouteType          mCurrentRoute     = null;
+	private   RouteDirectionType mCurrentDirection = null;
+	private   DetourType         mCurrentDetour    = null;
+	
+	/* (non-Javadoc)
+	 * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+	 */
+	@Override
+	public void startElement(String namespaceUri, String localName, String qualifiedName, Attributes attributes) throws SAXException {
+		if (qualifiedName.equalsIgnoreCase("resultSet")) {
+			mResultSet = new ResultSet();
+			if (attributes.getValue("errorMessage") != null)
+				mResultSet.setErrorMessage(attributes.getValue("errorMessage"));
+		} else if (qualifiedName.equalsIgnoreCase("route")) {
+			mCurrentRoute = new RouteType();
+			// required
+			mCurrentRoute.setRoute(Integer.parseInt(attributes.getValue("route")));
+			mCurrentRoute.setDesc(attributes.getValue("desc"));
+			mCurrentRoute.setType(attributes.getValue("type"));
+			// optional
+			if (attributes.getValue("detour") != null)
+				mCurrentRoute.setDetour(Boolean.parseBoolean(attributes.getValue("detour")));
+		} else if (qualifiedName.equalsIgnoreCase("dir")) {
+			mCurrentDirection = new RouteDirectionType();
+			mCurrentDirection.setDir(Integer.parseInt(attributes.getValue("dir")));
+			mCurrentDirection.setDesc(attributes.getValue("desc"));
+		} else if (qualifiedName.equalsIgnoreCase("stop")) {
+			StopType stop = new StopType();
+			stop.setDesc (attributes.getValue("desc"));
+			stop.setLocid(Integer.parseInt    (attributes.getValue("locid")));
+			stop.setLat  (Double .parseDouble (attributes.getValue("lat"  )));
+			stop.setLng  (Double .parseDouble (attributes.getValue("lng"  )));
+			stop.setSeq  (Integer.parseInt    (attributes.getValue("seq"  )));
+			stop.setTp   (Boolean.parseBoolean(attributes.getValue("tp"   )));
+			mCurrentDirection.getStop().add(stop);
+		} else if (qualifiedName.equalsIgnoreCase("detour")) {
+			mCurrentDetour = new DetourType();
+			mCurrentDetour.setBegin(Long.parseLong(attributes.getValue("begin")));
+			mCurrentDetour.setEnd  (Long.parseLong(attributes.getValue("end"  )));
+			mCurrentDetour.setId      (attributes.getValue("id"      ));
+			mCurrentDetour.setDesc    (attributes.getValue("desc"    ));
+			mCurrentDetour.setPhonetic(attributes.getValue("phonetic"));
+		} else {
+			Log.d(getClass().getSimpleName(), "There was an unknown XML element encountered: " + qualifiedName);
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void endElement(String namespaceUri, String localName, String qualifiedName) throws SAXException {
+		if (qualifiedName.equalsIgnoreCase("route")) {
+			// a route can belong to a resultSet or a detour
+			if (mCurrentDetour != null) 
+				mCurrentDetour.getRoute().add(mCurrentRoute);
+			else
+				mResultSet.getRoute().add(mCurrentRoute);
+			mCurrentRoute = null;
+		} else if (qualifiedName.equalsIgnoreCase("dir")) {
+			mCurrentRoute.getDir().add(mCurrentDirection);
+			mCurrentDirection = null;
+		} else if (qualifiedName.equalsIgnoreCase("detour")) {
+			mResultSet.getDetour().add(mCurrentDetour);
+			mCurrentDetour = null;
+		} else if (qualifiedName.equalsIgnoreCase("stop")) {
+			
+		}
+	}
+
+	
+	/*
+	 * 
+	 */
+	public ResultSet getResultSet() {
+		return mResultSet;	
+	}
+}
