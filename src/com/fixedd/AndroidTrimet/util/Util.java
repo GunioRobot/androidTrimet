b902072983637392fb1a8d8fd7d28ae1cd9095df
Jeremy Logan <jeremy.logan@gmail.com>
2009-12-23 13:36:55 -0800

    Added method for combining arrays of ints into a CSV string and got rid of the same for Lists.

--- src/com/fixedd/AndroidTrimet/util/Util.java
+++ src/com/fixedd/AndroidTrimet/util/Util.java
@@ -1,6 +1,5 @@
 package com.fixedd.AndroidTrimet.util;
 
-import java.util.List;
 
 /**
  * Utility functions used by the library internally.
@@ -8,20 +7,19 @@ import java.util.List;
  *
  */
 public class Util {
-
+	
 	/**
-	 * Convert a List<Integer> into a comma-separated string.
+	 * Convert an array of ints into a comma-separated string.
 	 * @param lst 
 	 * @return
 	 */
-	public static String listIntegersToString(List<Integer> lst) {
+	public static String arrayIntsToString(int... lst) {
 		String toReturn = "";
 		
-		for (int i=0; i<lst.size(); i++) {
-			toReturn = toReturn + lst.get(i).toString() + ",";
+		for (int i=0; i<lst.length; i++) {
+			toReturn = toReturn + Integer.toString(lst[i]) + ",";
 		}
 		
-		return toReturn.substring(0, -1);
+		return toReturn.subSequence(0, toReturn.length()-1).toString();
 	}
-	
 }
